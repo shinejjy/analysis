437a5d8203566312ba826c926dde9a09888e81bc
shinejjy <1263859380@qq.com>
2023-10-20 00:23:36 +0800

    3

--- sy2/jjy-project-1.py
+++ sy2/jjy-project-1.py
@@ -5,22 +5,27 @@ from matplotlib import pyplot as plt
 
 class Process:
     def __init__(self, kernel_size=(3, 3)):
+        if kernel_size[0] % 2 == 0 or kernel_size[1] % 2 == 0:
+            raise ValueError("Kernel size must be odd in both dimensions.")
+        self.kernel_size = kernel_size
         self.kernel = np.ones(kernel_size, np.uint8)
 
     def erode(self, img):
-        img_pad = np.pad(img, (1, 1), constant_values=(255, 255))  # 周围填充最大值
+        pad_size = (self.kernel_size[0] // 2, self.kernel_size[1] // 2)
+        img_pad = np.pad(img, pad_size, constant_values=(255, 255))  # 周围填充最大值
         eroded_image = img.copy()
         for i in range(img.shape[0]):
             for j in range(img.shape[1]):
-                eroded_image[i][j] = min(img_pad[i: i + 3, j: j + 3].flatten())
+                eroded_image[i][j] = np.min(img_pad[i: i + self.kernel_size[0], j: j + self.kernel_size[1]].flatten())
         return eroded_image
 
     def dilate(self, img):
-        img_pad = np.pad(img, (1, 1), constant_values=(0, 0))  # 周围填充最大值
+        pad_size = (self.kernel_size[0] // 2, self.kernel_size[1] // 2)
+        img_pad = np.pad(img, pad_size, constant_values=(0, 0))  # 周围填充最小值
         dilated_image = img.copy()
         for i in range(img.shape[0]):
             for j in range(img.shape[1]):
-                dilated_image[i][j] = max(img_pad[i: i + 3, j: j + 3].flatten())
+                dilated_image[i][j] = np.max(img_pad[i: i + self.kernel_size[0], j: j + self.kernel_size[1]].flatten())
         return dilated_image
 
     def open(self, img):
@@ -53,41 +58,46 @@ def main():
     closed_image = process.close(gray_image)
     gradient_image = process.gradient(gray_image)
 
-    # 创建一个Matplotlib Figure对象
-    fig = plt.figure(figsize=(12, 10))
-
-    # 在Figure中添加子图
-    plt.subplot(2, 3, 1)
+    # 显示原始图像
+    plt.figure(figsize=(8, 6))
     plt.imshow(gray_image, cmap='gray')
     plt.title('Original Image')
     plt.axis('off')
+    plt.show()
 
-    plt.subplot(2, 3, 2)
+    # 显示腐蚀后的图像
+    plt.figure(figsize=(8, 6))
     plt.imshow(eroded_image, cmap='gray')
     plt.title('Eroded Image')
     plt.axis('off')
+    plt.show()
 
-    plt.subplot(2, 3, 3)
+    # 显示膨胀后的图像
+    plt.figure(figsize=(8, 6))
     plt.imshow(dilated_image, cmap='gray')
     plt.title('Dilated Image')
     plt.axis('off')
+    plt.show()
 
-    plt.subplot(2, 3, 4)
+    # 显示开运算后的图像
+    plt.figure(figsize=(8, 6))
     plt.imshow(opened_image, cmap='gray')
     plt.title('Opened Image')
     plt.axis('off')
+    plt.show()
 
-    plt.subplot(2, 3, 5)
+    # 显示闭运算后的图像
+    plt.figure(figsize=(8, 6))
     plt.imshow(closed_image, cmap='gray')
     plt.title('Closed Image')
     plt.axis('off')
+    plt.show()
 
-    plt.subplot(2, 3, 6)
+    # 显示梯度图像
+    plt.figure(figsize=(8, 6))
     plt.imshow(gradient_image, cmap='gray')
     plt.title('Gradient Image')
     plt.axis('off')
-
-    plt.tight_layout()  # 自动调整子图布局，以防止重叠
     plt.show()
 
 
